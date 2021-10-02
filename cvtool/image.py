2ee3c02b6d3ba32f5c82caaab2fdd3e7f72c3afa
QZLin <qzlin01@163.com>
2021-10-02 09:54:48 +0800

    add crop tool

--- cvtool/image.py
+++ cvtool/image.py
@@ -15,3 +15,39 @@ def cv2pil(image: np.ndarray):
 def test_img(img, print_pos=False):
     imshow('test', img)
     waitKey()
+
+
+def crop_img(img: np.ndarray, file_name=None):
+    if file_name is None:
+        file_name = 'crop.png'
+
+    class Data:
+        first_click = None
+        second_click = None
+
+    def handle_image(xs, ys):
+        print(xs, ys)
+        imwrite(file_name, img[ys[0]:ys[1], xs[0]:xs[1]])
+
+    def on_mouse_action(event, x, y, flags, param):
+        if event == cv.EVENT_LBUTTONUP:
+            # print(event, x, y)
+            if Data.first_click is None:
+                Data.first_click = (x, y)
+            else:
+                Data.second_click = (x, y)
+
+                x1, y1 = Data.first_click
+                x2, y2 = Data.second_click
+                xs, ys = [x1, x2], [y1, y2]
+                xs.sort()
+                ys.sort()
+                handle_image(xs, ys)
+
+                Data.first_click = None
+                Data.second_click = None
+
+    name = 'crop'
+    imshow(name, img)
+    cv.setMouseCallback(name, on_mouse_action)
+    waitKey()
