39469b16b8c5e71101a789df47645c8d7e39c5c6
QZLin <qzlin01@163.com>
2022-02-21 20:38:47 +0800

    !Break Change: Adjust project structure to python package, extract path less asset as a module

--- cvtool/tool.py
+++
@@ -1,5 +0,0 @@
-import cvtool.image as im
-import cvtool.draw
-import cvtool.template_match as tm
-import cvtool.no_path_asset as npa
-import cvtool.positions as pos
---
+++ tool.py
@@ -0,0 +1,5 @@
+import image as im
+import draw
+import template_match as tm
+import pathless_asset.module as pla
+import positions as pos
